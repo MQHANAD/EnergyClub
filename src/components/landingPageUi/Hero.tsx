f5a041bcf42966ef9d84ae16bfd2c83929e74859
Mohanad <s202024100@kfupm.edu.sa>
2025-11-19 19:25:13 +0300

    improved landing page

--- src/components/landingPageUi/Hero.tsx
+++ src/components/landingPageUi/Hero.tsx
@@ -5,7 +5,7 @@ import { motion, type Variants } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 export default function Hero() {
-  const { t } = useI18n();
+  const { t, dir } = useI18n();
   const { ref, inView } = useInView({
     threshold: 0.2,
     triggerOnce: true,
@@ -16,7 +16,7 @@ export default function Hero() {
   };
 
   return (
-    <section id="about" className="bg-[#25818a] md:py-36 py-16">
+    <section id="about" dir={dir} className="bg-[#25818a] md:py-36 py-16">
       <div
         ref={ref}
         className="md:mx-16 flex max-w-8xl flex-col items-start justify-center px-4 md:px-8"
@@ -25,14 +25,14 @@ export default function Hero() {
           initial={{ opacity: 0, y: 20 }}
           animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
           transition={{ duration: 0.6 }}
-          className="text-4xl font-bold text-white mb-6 text-left w-50 md:w-80"
+          className="text-4xl font-bold text-white mb-6 w-70 md:w-80"
         >
           {t("hero.titleEnergyHub")}
         </motion.h2>
 
         <motion.p
           variants={itemVariants}
-          className="mx-auto mt-6 text-balance text-base leading-relaxed text-white sm:text-lg md:text-xl text-left"
+          className="mt-6 text-balance text-base leading-relaxed text-white sm:text-lg md:text-xl"
         >
           {t("hero.tagline")}
         </motion.p>
